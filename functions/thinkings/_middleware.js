33f97f6fd0858d5d1a7d238699b03be5e5f9042b
smit-nagrale <smitnagralehacker@gmail.com>
2026-08-30 04:28:47 +0530

    Create _middleware.js

---
+++ functions/thinkings/_middleware.js
@@ -0,0 +1,8 @@
+import { verifySession } from "../utils/session.js";
+
+export async function onRequest(context) {
+  const { request, env, next } = context;
+  const valid = await verifySession(request, env);
+  if (valid) return next();
+  return Response.redirect(new URL("/", request.url), 302);
+}
